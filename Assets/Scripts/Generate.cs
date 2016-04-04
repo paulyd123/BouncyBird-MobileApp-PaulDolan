df697e631df5397d067df1886c1e16189a3427c6
paulyd123 <G00297086@gmit.ie>
2016-04-04 19:40:22 +0100

    menu

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+	public GameObject trees;
+	int score = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 2.5f);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.black;
+		GUILayout.Label(" Score: " + score.ToString());
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(trees);
+		score++;
+	}
+}
